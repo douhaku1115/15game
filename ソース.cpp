86fe9975dc78c7d3c17f347f3715e46245493209
douhaku <douhaku1115@gmail.com>
2020-07-19 09:25:48 +0900

    check変更

--- ソース.cpp
+++ ソース.cpp
@@ -9,6 +9,9 @@
 #define BOARD_WIDTH 4
 #define BOARD_HEIGHT 4
 #define  BLANK_NUMBER 16
+typedef struct {
+	int x, y;
+}VEC2;
 
 int cx = 3, cy = 3;
 char cellAA[][2 + 1] = {
@@ -40,19 +43,6 @@ void init(){
 	for (int y = 0; y < BOARD_HEIGHT; y++) 
 		for (int x = 0; x < BOARD_WIDTH; x++) 
 			board[y][x] = y * BOARD_WIDTH + x+1;
-
-	printf("+--+--+--+--+\n");
-	for (int y = 0; y < BOARD_HEIGHT; y++) {
-		for (int x = 0; x < BOARD_WIDTH; x++)
-			if (board[y][x] == BLANK_NUMBER)
-				printf("|  ");
-			else
-				printf("|%2d", board[y][x]);
-
-		printf("|\n");
-		printf("+--+--+--+--+\n");
-	
-	}
 }
 void shuffle() {
 
@@ -63,17 +53,24 @@ void move() {
 int getField(int _x, int _y) {
 	return board[_y][_x];
 }
-int getSpace() {
-	for (int y = 0; y < BOARD_HEIGHT; y++)
-		for (int x = 0; x < BOARD_WIDTH; x++)
-			if (board[y][x] == BLANK_NUMBER)
-				return board[y][x];
+
+VEC2 getSBlankPos() {
+	VEC2 blankPos = { -1,-1 };
+	for (int j = 0; j < BOARD_HEIGHT; j++)
+		for (int i = 0; i < BOARD_WIDTH; i++)
+			if (board[j][i] == BLANK_NUMBER)
+
+				return blankPos = { i,j };
 }
 void swap(int *x,int *y) {
 	int* temp = x;
 	x = y;
 	y = temp;
 
+}
+bool check(VEC2 pos) {
+	if(pos.x!=0 && pos.y!=0)
+	return true;
 }
 int main() {
 	init();  //‰ŠúÝ’è 
@@ -82,14 +79,17 @@ int main() {
 	move();
 
 	while (1) {
+		VEC2 x, y;
 		if (_kbhit()) {
 
 
 			switch (_getch()) {
 			case'w':
+				if (check(getSBlankPos()) != 0)
+					; break;
 				board[2][3] = 16; board[3][3] = 12; break;
 			case's':
-				; break;
+				if(check(getSBlankPos() )!=0); break;
 			case'd':
 				; break;
 			case'a':
